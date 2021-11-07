46a662d74ccaef6947d886023cc7fd9ede80c196
Utkarsh <Utkarshsinghclash@gmail.com>
2021-11-07 20:21:38 +0530

    the second change

--- code.cpp
+++ code.cpp
@@ -2,6 +2,8 @@
 #include<stdlib.h>
 #include<time.h>
 #include <dirent.h>
+// used for rmdir
+//#include<ftw.h>
 using namespace std;
 //change the directory
 void changedir(){
@@ -44,23 +46,28 @@ cout<<endl;
 }
 // lists all the files in the directory
 int ls()
+{   
+struct dirent *de;
+DIR *dr = opendir(".");
+if (dr == NULL)
 {
-     struct dirent *de;
-
-    DIR *dr = opendir(".");
-
-    if (dr == NULL)
-    {
-        printf("Could not open current directory" );
-        return 0;
-    }
-
-    while ((de = readdir(dr)) != NULL)
-            printf("%s\n", de->d_name);
-
-    closedir(dr);
-    return 0;
+printf("Could not open current directory");
+return 0;
 }
+while ((de = readdir(dr)) != NULL)
+printf("%s\n", de->d_name);
+closedir(dr);
+return 0;
+}
+// makes a file in the directory
+// not for every file 
+void makefile(char* filename){
+    FILE *fp = fopen(filename, "w");
+}
+// removes a empty directory
+//void removedir(char* dirname){
+//
+//}
 
 int main(){
 string cmd;
@@ -70,31 +77,48 @@ int size=name.size();
 char array[size+1];
     int num=9;
     int ch;
-    cout<<"	WELCOME TO THE LINUX SHELL" <<endl;
+    cout<<" WELCOME TO THE LINUX SHELL" <<endl;
     cout<<" 1: for making directory" <<endl;
     cout<<" 2: for displaying the present work directory" <<endl;
     cout<<" 3: list all the files in that directory" <<endl;
     cout<<" 4: change the working directory" <<endl;
     cout<<" 5: deletes a file in a directory" <<endl;
+    cout<<" 6. makes a file in a directory"<<endl;
+//    cout<<" 7. deletes a directory" <<endl;
     while(num){
     cout<<"enter the choice"<<endl;
     cin>>ch;
     switch(ch){
     case 1:     cout<<"enter the command"<<endl;
                 cin>>cmd>>name;
+                if(cmd=="mkdir"){
                 strcpy(array,name.c_str());
                 makedir(array);
+				}
+				else{
+				cout<<cmd<<" :Command Not found"<<endl;	
+				}
                 break;
     case 2:     cout<<"enter the command :-"<<endl;
                 cin>>cmd;
-                pwd();
+                if(cmd=="pwd"){
+                 pwd();	
+				}
+				else{
+				cout<<cmd<<" :Command Not found"<<endl;		
+				}
                 break;
     case 3:     cout<<"enter the command :-"<<endl;
                 cin>>cmd;
+                if(cmd=="ls"){
                 ls();
+				}
+				else{
+				cout<<cmd<<" :Command Not found"<<endl;		
+				}
                 break;
 	case 4:     cout<<"enter the command :-"<<endl;
-				cin>>name>>cmd;
+				cin>>cmd>>name;
 				changedir();
 				break;	
 	case 5:     cout<<"enter the command :-"<<endl;
@@ -103,7 +127,10 @@ char array[size+1];
 				delfile(array);
 				break;
 	case 6:		cout<<"enter the command :-"<<endl;
-				
+                cin>>cmd>>name;
+                strcpy(array,name.c_str());
+                makefile(array);
+                break;
 	default:    cout<<"You entered a wrong choice"<<endl;
 				break;			
     }
